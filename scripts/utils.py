84d3d25d721a97131dcb14b422466af28598bc94
Asier Galicia <a.galiciamartinez@student.tudelft.nl>
2020-05-25 18:28:16 +0200

    Implemented useful functions such as pauly matrices and qubit initialization.

---
+++ scripts/utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+sx = np.array([[0, 1], [1, 0]], dtype=complex)
+sy = np.array([[0, -1j], [1j, 0]], dtype=complex)
+sz = np.array([[1, 0], [0, -1]], dtype=complex)
+si = np.eye(2, dtype=complex)
+
+
+def init_qubit(n):
+    """Returns a qubit with the bloch vector pointing in the given direction. n
+    must be a numpy array and it is not required to be normalized (it is
+    normalized before creating the state)
+
+    """
+    return (si + sx * n[0] + sy * n[1] + sz * n[2]) / 2
