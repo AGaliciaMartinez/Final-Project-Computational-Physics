Fix bug. Normalization not being computed correctly.

scripts/utils.py
@@ -17,5 +17,5 @@ def init_qubit(bloch_direction):
     else:
         n = bloch_direction
 
-    n = np.linalg.norm(n)
+    n = n / np.linalg.norm(n)
     return (si + sx * n[0] + sy * n[1] + sz * n[2]) / 2