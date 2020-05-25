da20acf22f398ad3ccabd7373804a925c8f6c0db
Asier Galicia <a.galiciamartinez@student.tudelft.nl>
2020-05-25 18:31:45 +0200

    Now init_qubit can use non-normalized vectors to create a pure state.

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,10 +5,11 @@ sz = np.array([[1, 0], [0, -1]], dtype=complex)
 si = np.eye(2, dtype=complex)
 
 
-def init_qubit(n):
+def init_qubit(bloch_direction):
     """Returns a qubit with the bloch vector pointing in the given direction. n
     must be a numpy array and it is not required to be normalized (it is
     normalized before creating the state)
 
     """
+    n = np.linalg.norm(bloch_direction)
     return (si + sx * n[0] + sy * n[1] + sz * n[2]) / 2
