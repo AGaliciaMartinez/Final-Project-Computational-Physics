7485627baf520e12069a5d9c33c903b5744ce4a5
Asier Galicia <a.galiciamartinez@student.tudelft.nl>
2020-06-09 16:59:42 +0200

    Inclued script for plots of NV-centre.

--- examples/plot_decoherence_NV.py
+++ examples/plot_decoherence_NV.py
@@ -28,25 +28,33 @@ def fidelity(t, A, T_2, n):
     return 1 / 2 + A / 2 * np.exp(-(t / T_2)**n)
 
 
-N = [1]
+N = [1, 2, 4, 8, 16]
+colors = ['purple', 'orange', 'green', 'blue', 'red']
 
-plt.figure(figsize=(6, 3))
-for i in N:
+plt.figure(1, figsize=(6, 3))
+Ts_mean = []
+Ts_std = []
+for i, n in enumerate(N):
     time = np.load('../script_output/nv_deco_time.npz')['time']
-    data = np.load(f'../script_output/nv_deco_{i}.npz')['result']
+    data = np.load(f'../script_output/nv_deco_{n}.npz')['result']
     mean = data[0, 0]
     std = data[1, 0]
 
     fit_mean, fit_std = curve_fit(fidelity, time, mean, p0=(1, 300, 3))
+    fit_err = np.sqrt(np.diag(fit_std))
 
-    plt.errorbar(time, mean, std, label=f'N={i}')
+    plt.errorbar(time, mean, std, label=f'N={n}', color=colors[i])
+    plt.plot(time, fidelity(time, *fit_mean), '--', color=colors[i])
+
     A, T_2, n = fit_mean
-    plt.plot(time, fidelity(time, A, T_2, n))
 
-    print(f'N={i}')
-    print(f'A={A} pm {fit_std[0,0]}')
-    print(f'T_2={T_2} pm {fit_std[1,1]}')
-    print(f'n={n} pm {fit_std[2,2]}')
+    Ts_mean.append(T_2)
+    Ts_std.append(fit_err[1])
+    print(f'N={n}')
+    print(f'A={A} pm {fit_err[0]}')
+    print(f'T_2={T_2} pm {fit_err[1]}')
+    print(f'n={n} pm {fit_err[2]}')
+    print('')
 
 plt.title('Dynamical Decoupling on NV-centre')
 plt.ylabel(r'$P_x$')
@@ -55,4 +63,29 @@ plt.xscale('log')
 plt.tight_layout()
 plt.legend()
 plt.savefig('../presentation/images/nv_decoherence.svg')
+
+
+# Fit of Tau
+def f(N, A, exponent):
+    return A * N**exponent
+
+
+fit_mean, fit_std = curve_fit(lin, N, Ts_mean, p0=(1, 2 / 3), sigma=Ts_std)
+fit_err = np.sqrt(np.diag(fit_std))
+
+print(f'A = {fit_mean[0]} pm {fit_err[0]}')
+print(f'exponen = {fit_mean[1]} pm {fit_err[1]}')
+
+plt.figure(2, figsize=(6, 3))
+plt.errorbar(N, Ts_mean, Ts_std)
+plt.plot(N, lin(N, *fit_mean))
+plt.tight_layout()
+plt.title('Scaling of $T_2$ with Dynamical Decoupling')
+plt.ylabel(r'$T_2$')
+plt.xlabel(r'$N$')
+plt.xscale('log')
+plt.yscale('log')
+
+np.savez('../script_output/nv_deco_T2.npz', T2_mean=Ts_mean, T2_std=Ts_std)
+
 plt.show()
